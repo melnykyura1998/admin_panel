490c5bbec252550a9ec635e937d28fe80ec9a3d3
yuramelnyk <melnykyura1998@gmail.com>
2025-03-27 19:37:47 +0200

    fix: zro-2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+zxczxczxc
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
